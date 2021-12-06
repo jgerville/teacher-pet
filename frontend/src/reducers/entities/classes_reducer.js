450b04a98d8cbeeaad8ee7cfcc6e345863a55479
Julian Erville <jerville@gmail.com>
2021-12-06 14:41:25 -0500

    Add classes reducer and entities reducer

---
+++ frontend/src/reducers/entities/classes_reducer.js
@@ -0,0 +1,10 @@
+const classesReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch(action.type) {
+
+    default: return state;
+  }
+}
+
+export default classesReducer;
---
+++ frontend/src/reducers/entities_reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import classesReducer from "./entities/classes_reducer";
+
+
+const entitiesReducer = combineReducers({
+  classes: classesReducer,
+})
+
+export default entitiesReducer;
