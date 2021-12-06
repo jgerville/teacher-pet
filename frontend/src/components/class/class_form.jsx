628b426a2479f425eeb1e1a79aba0402583f9254
Julian Erville <jerville@gmail.com>
2021-12-06 17:55:13 -0500

    Add class form

--- frontend/src/components/class/class_form.jsx
+++ frontend/src/components/class/class_form.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 const ClassForm = ({ createClass }) => {
-  
+  const [name, setName] = useState("");
+  const [subject, setSubject] = useState("");
+  // const [students, setStudents] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  }
+
   return (
     <div className="outer-modal">
       <form className="class-form">
-        <label htmlFor="title">Title</label>
-        <input type="text" id="title" placeholder="Title" />
+        <i className="fas fa-times" />
+        <input
+          type="text"
+          id="name"
+          placeholder="Class name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          type="text"
+          id="subject"
+          placeholder="Subject"
+          onChange={(e) => setSubject(e.target.value)}
+          value={subject}
+        />
+        <button onClick={handleSubmit}>Add Class</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 ClassForm.propTypes = {
   createClass: PropTypes.func.isRequired,
-}
+};
 
-export default ClassForm
+export default ClassForm;
