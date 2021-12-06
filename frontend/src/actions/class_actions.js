2f6692b05dbd8206c581887273f022d41f5ee69e
Julian Erville <jerville@gmail.com>
2021-12-06 15:14:14 -0500

    Add show, index, post class actions, and add class error handling

--- frontend/src/actions/class_actions.js
+++ frontend/src/actions/class_actions.js
@@ -1,13 +1,40 @@
-export const RECEIVE_CLASS = 'RECEIVE_CLASS';
-export const RECEIVE_CLASSES = 'RECEIVE_CLASSES';
+import * as ClassAPI from "../util/class_api_util";
+export const RECEIVE_CLASS = "RECEIVE_CLASS";
+export const RECEIVE_CLASSES = "RECEIVE_CLASSES";
+export const REMOVE_CLASS = "REMOVE_CLASS";
+export const RECEIVE_CLASS_ERRORS = "RECEIVE_CLASS_ERRORS";
 
 const receiveClass = (klass) => ({
   type: RECEIVE_CLASS,
   klass,
-})
+});
 
 const receiveClasses = (classes) => ({
   type: RECEIVE_CLASSES,
   classes,
-})
+});
 
+const removeClass = (klass) => ({
+  type: REMOVE_CLASS,
+  klass,
+});
+
+const receiveErrors = (errors) => ({
+  type: RECEIVE_CLASS_ERRORS,
+  errors,
+});
+
+export const getClass = (classId) => (dispatch) =>
+  ClassAPI.showClass(classId)
+    .then((klass) => dispatch(receiveClass(klass)))
+    .catch((err) => dispatch(receiveErrors(err)));
+
+export const getClassesForOneTeacher = (userId) => (dispatch) =>
+  ClassAPI.getClassesByUserId(userId)
+    .then((classes) => dispatch(receiveClasses(classes)))
+    .catch((err) => receiveErrors(err));
+
+export const createClass = (klass) => (dispatch) =>
+  ClassAPI.createClass(klass)
+    .then((newClass) => dispatch(receiveClass(newClass)))
+    .catch((err) => dispatch(receiveErrors(err)));
---
+++ frontend/src/reducers/errors/class_errors_reducer.js
@@ -0,0 +1,20 @@
+import { RECEIVE_CLASS, RECEIVE_CLASSES, RECEIVE_CLASS_ERRORS, REMOVE_CLASS } from "../../actions/class_actions";
+
+const classErrorsReducer = (state = [], action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_CLASS_ERRORS:
+      return action.errors;
+    case RECEIVE_CLASS:
+      return [];
+    case RECEIVE_CLASSES:
+      return [];
+    case REMOVE_CLASS:
+      return [];
+    default: 
+      return state
+  }
+}
+
+export default classErrorsReducer;
