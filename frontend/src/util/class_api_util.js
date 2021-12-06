96e011e69be28bf6edb1b7ad60d5eaa2d6d9b50d
Julian Erville <jerville@gmail.com>
2021-12-06 14:56:24 -0500

    Add show class and get classes by user id

--- frontend/src/util/class_api_util.js
+++ frontend/src/util/class_api_util.js
@@ -5,3 +5,14 @@ export const createClass = async (klass) => {
   return response.data;
 };
 
+export const showClass = async (classId) => {
+  const response = await axios.get(`api/classes/${classId}`);
+  return response.data;
+};
+
+export const getClassesByUserId = async (userId) => {
+  const response = await axios.get(`api/classes/byUser/${userId}`);
+  // the above route will probably be different
+  return response.data;
+}
+
