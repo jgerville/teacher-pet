ccc6030908e7adc9e713a630b3f2c5e36e333714
Julian Erville <jerville@gmail.com>
2021-12-06 15:20:22 -0500

    Complete classes reducer

--- frontend/src/reducers/entities/classes_reducer.js
+++ frontend/src/reducers/entities/classes_reducer.js
@@ -1,9 +1,20 @@
+import { RECEIVE_CLASS, RECEIVE_CLASSES, REMOVE_CLASS } from "../../actions/class_actions";
+
 const classesReducer = (state = {}, action) => {
   Object.freeze(state);
+  const nextState = Object.assign({}, state);
 
   switch(action.type) {
-
-    default: return state;
+    case RECEIVE_CLASS:
+      nextState[action.klass.id] = action.klass;
+      return nextState;
+    case RECEIVE_CLASSES:
+      return action.classes;
+    case REMOVE_CLASS:
+      delete nextState[action.classId];
+      return nextState;
+    default: 
+      return state;
   }
 }
 
