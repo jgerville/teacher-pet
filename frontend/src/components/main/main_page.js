ce3107de4935de59e78c0947e1b8d1e50df0daf9
cjc473 <chris.cheasty3@gmail.com>
2021-12-06 11:10:12 -0800

    midway through frontend auth

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Teacher Pet</h1>
+        <footer>
+          A free timesaving app for teachers.
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
