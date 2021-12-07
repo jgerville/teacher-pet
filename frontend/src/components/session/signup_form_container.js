e818c84ac5397e4976f5a049fc28049d8613601c
cjc473 <chris.cheasty3@gmail.com>
2021-12-06 16:16:07 -0800

    initial modal code complete

--- frontend/src/components/session/signup_form_container.js
+++ frontend/src/components/session/signup_form_container.js
@@ -2,20 +2,14 @@ import { connect } from 'react-redux';
 import { signup } from '../../actions/session_actions';
 import SignupForm from './signup_form';
 
-const mapStateToProps = (state) => {
-  return {
-    signedIn: state.session.isSignedIn,
-    errors: state.errors.session
-  };
-};
+const mapStateToProps = ({errors, session}) => ({
+  signedIn: session.isSignedIn,
+  errors: errors.session,
+  formType: 'signup'
+});
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    signup: user => dispatch(signup(user))
-  }
-}
+const mapDispatchToProps = (dispatch) => ({
+  signup: user => dispatch(signup(user))
+})
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SignupForm);
+export default connect(mapStateToProps, mapDispatchToProps)(SignupForm);
