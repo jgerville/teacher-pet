06d2dca6760c0a6ff25e779ec0023dce7b77f562
Julian Erville <jerville@gmail.com>
2021-12-06 20:40:02 -0500

    prepare keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://ecbell:hrLb0ztO844X30bZ@cluster0.me9xc.mongodb.net/teacherPet?retryWrites=true&w=majority',
-  secretOrKey: 'secret'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
