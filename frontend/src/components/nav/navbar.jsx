b54e1e58ca12f3730d1089cf6e6b99aa2a5593af
cjc473 <chris.cheasty3@gmail.com>
2021-12-07 15:14:57 -0800

    add photos for background testing

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -18,14 +18,14 @@ class NavBar extends React.Component {
     if (this.props.loggedIn) {
       return (
         <div>
-          <button onClick={this.logoutUser}>Logout</button>
+          <button class="nav-button" onClick={this.logoutUser}>Logout</button>
         </div>
       );
     } else {
       return (
-        <div>
-          <button onClick={() => this.props.openModal('signup')}>Sign up</button>
-          <button onClick={() => this.props.openModal('login')}>Login</button>
+        <div class="nav-button-container">
+          <button class="nav-button" onClick={() => this.props.openModal('signup')}>Sign up</button>
+          <button class="nav-button" onClick={() => this.props.openModal('login')}>Login</button>
           {/* <Link to={'/signup'}>Signup</Link>
           <Link to={'/login'}>Login</Link> */}
         </div>
@@ -36,8 +36,8 @@ class NavBar extends React.Component {
   render() {
     return (
       <div class="navbar navbar-expand-lg fixed-top">
-        <h1>Teacher's Pet</h1>
-        {this.getLinks()}
+        <h2 class="nav-header">Teacher's Pet</h2>
+        <div >{this.getLinks()}</div>
       </div>
     );
   }
