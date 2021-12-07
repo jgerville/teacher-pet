65c23e1ef9f18c6a6543822ff2c979bc1a373973
Julian Erville <jerville@gmail.com>
2021-12-07 13:27:29 -0500

    Add form for adding many students

--- frontend/src/components/students/form/student_form.jsx
+++ frontend/src/components/students/form/student_form.jsx
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import StudentFormHeader from "./student_form_header";
+import AddManyForm from "./add_many_form";
 
 const StudentForm = (props) => {
   const [currentTab, setCurrentTab] = useState("addMany");
+  const switchToMany = () => {
+    if (currentTab === "addOne") {
+      setCurrentTab("addMany");
+    }
+  };
+  const switchToOne = () => {
+    if (currentTab === "addMany") {
+      setCurrentTab("addOne");
+    }
+  };
 
   return (
     <div className="student-form-container">
-      <StudentFormHeader
-        addMany={() => setCurrentTab("addMany")}
-        addOne={() => setCurrentTab("addOne")}
-      />
+      <StudentFormHeader addMany={switchToMany} addOne={switchToOne} />
+      {currentTab === "addMany" ? (
+        <AddManyForm />
+      ) : (
+
+      )}
     </div>
   );
 };
--- frontend/src/components/students/form/student_form_header.jsx
+++ frontend/src/components/students/form/student_form_header.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
 const StudentFormHeader = ({ addMany, addOne }) => {
   return (
     <div className="student-form-header">
-      <div className="add-many-tab">
+      <div className="add-many-tab" onClick={addMany}>
         <span>Add many</span>
       </div>
-      <div className="add-one-tab">
+      <div className="add-one-tab" onClick={addOne}>
         <span>Add one</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
 StudentFormHeader.propTypes = {
+  addMany: PropTypes.func.isRequired,
+  addOne: PropTypes.func.isRequired,
+};
 
-}
-
-export default StudentFormHeader
+export default StudentFormHeader;
