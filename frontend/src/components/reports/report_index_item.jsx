d6ee20d6658e8ed0414382ecfd5e0ffcd9a172c5
Julian Erville <jerville@gmail.com>
2021-12-08 11:48:48 -0500

    Add report index and incomplete indexItem

---
+++ frontend/src/components/reports/report_index_item.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ReportIndexItem = ({ report }) => {
+  return (
+    <div className="report-index-item">
+      edit this 
+    </div>
+  )
+}
+
+ReportIndexItem.propTypes = {
+  report: PropTypes.object,
+}
+
+export default ReportIndexItem
--- frontend/src/util/array_util.js
+++ frontend/src/util/array_util.js
@@ -2,3 +2,8 @@ export const sortAlphabetically = (arrayOfObjects, propertyToSortBy) =>
   arrayOfObjects.sort((a, b) =>
     a[propertyToSortBy][0].toLowerCase() > b[propertyToSortBy][0].toLowerCase() ? 1 : -1
   );
+
+export const sortByDate = (arrayOfObjects, propertyToSortBy) =>
+  arrayOfObjects.sort((a, b) =>
+    new Date(a[propertyToSortBy]) - new Date(b[propertyToSortBy])
+  )
