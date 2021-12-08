9323326caaccba2cdb20e7500eae2feffc6f1e8e
Julian Erville <jerville@gmail.com>
2021-12-07 19:36:10 -0500

    Add student api util

---
+++ frontend/src/util/student_api_util.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export const getStudentsForUser = async () => {
+  const response = await axios.get("api/students");
+  return response.data;
+};
+
+export const showStudent = async (studentId) => {
+  const response = await axios.get(`api/students/${studentId}`);
+  return response.data;
+};
+
+export const createStudent = async (student) => {
+  const response = await axios.post("api/students", student);
+  return response.data;
+};
+
+export const editStudent = async (student) => {
+  const response = await axios.patch(
+    `api/students/${student._id}/edit}`,
+    student
+  );
+  return response.data;
+};
+
+export const deleteStudent = async (studentId) => {
+  const response = await axios.delete(`api/students/${studentId}`);
+  return response.data;
+};
