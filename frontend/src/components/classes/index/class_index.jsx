a0566863be0227a9469eca6469df91475141e906
Julian Erville <jerville@gmail.com>
2021-12-07 19:12:04 -0500

    Add loading and error handling to API calls

--- frontend/src/components/classes/index/class_index.jsx
+++ frontend/src/components/classes/index/class_index.jsx
@@ -1,15 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import ReactLoading from 'react-loading';
 import ClassIndexItem from "./class_index_item";
 import { sortAlphabetically } from "../../../util/array_util";
 
 const ClassIndex = ({ classes, getClasses }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  
   useEffect(() => {
-    getClasses();
+    const fetchClasses = async () => {
+      try {
+        setIsLoading(true);
+        setError("");
+        await getClasses();
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setError("There was an error finding your classes.")
+      }
+    }
+    fetchClasses();
+    return () => {
+      setError("");
+      setIsLoading(false);
+    }
   }, [getClasses]);
 
   return (
     <div className="class-index">
+      {error && <p>{error}</p>}
+      {isLoading && <ReactLoading type={"spinningBubbles"} color={"#808080"} height={50} width={50} />}
       {classes ? (
         <ul className="class-list">
           {sortAlphabetically(classes, "name").map((klass) => (
--- frontend/src/components/pages/class_show_page.jsx
+++ frontend/src/components/pages/class_show_page.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import ClassShowHeader from '../classes/show/class_show_header';
+import ReactLoading from 'react-loading';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { getClass } from '../../actions/class_actions';
@@ -8,17 +9,35 @@ import AddItemButton from '../reusable/add_item_button';
 
 const ClassShowPage = ({ klass, classId, getClass }) => {
   const [isCreatingStudent, setIsCreatingStudent] = useState(false);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  
 
   useEffect(() => {
-    getClass(classId);
+    const fetchClass = async () => {
+      try {
+        setIsLoading(true);
+        setError("");
+        await getClass(classId);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setError("There was an error finding this class.");
+      }
+    }
+    fetchClass();
+    return () => {
+      setError("");
+    }
   }, [classId, getClass])
 
   const openStudentForm = () => setIsCreatingStudent(true);
   const closeStudentForm = () => setIsCreatingStudent(false);
-  console.log("shown: ", klass)
 
   return (
     <main>
+      {error && <p>{error}</p>}
+      {isLoading && <ReactLoading type={"spinningBubbles"} color={"#808080"} height={50} width={50} />}
       {klass ? (
         <>
           <ClassShowHeader klass={klass} />
