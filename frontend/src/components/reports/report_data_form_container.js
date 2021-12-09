4ba7f3ffe76de0bff40cb7840949cdb4094fd199
cjc473 <chris.cheasty3@gmail.com>
2021-12-09 08:58:12 -0800

    complete report data form

--- frontend/src/components/reports/report_data_form_container.js
+++ frontend/src/components/reports/report_data_form_container.js
@@ -1,12 +1,13 @@
 import { connect } from "react-redux";
 import ReportForm from "./report_form";
+import { createReportData } from "../../actions/report_data_actions";
 
 const mapStateToProps = (state, ownProps) => ({
   student: state.entities.students[ownProps.match.params.studentId]
 })
 
 const mapDispatchToProps = dispatch => ({
-  
+  createReportData: (reportData) => dispatch(createReportData(reportData))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ReportForm)
