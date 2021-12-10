95882b87cf84df7aae1a26087eddd6d8ba72fdc2
ecbell <ebell691@gmail.com>
2021-12-10 09:50:08 -0600

    update find and replace util

--- frontend/src/util/report_string_util.js
+++ frontend/src/util/report_string_util.js
@@ -1,51 +1,8 @@
-// LEGEND
-// %subject pronoun% => He, She, They
-// %fname% => first name of student
-// %subject% => subject category of class, i.e. math or history
-// %class name% => name of class
-// %posessive pronoun% => her, his, their
-// %object pronoun% => her, him, them
-// %category% => teacher-made category
-
 const { genderPronouns } = require("../../../util/report_data_keys")
-
-
-// VERBS => Conjugate based on singular vs plural (i.e. based on subj choice.)
-// &is/are&
-// &has/have&
-// [distracts/distract]
-// [completes/complete]
-// [fulfill/fulfills]
-// [misses / miss]
-// &does/do&
-// &shows/show&
-// [lacks/lack]
-// [participates/participate]
-// [needs/need]
-
-
-// const replacement = (whateverStr, someVar) => {
-//   str.replace(whateverStr, someVar)
-// }
-
-// first check for type of pronoun. set it = to variable, verb index => 
-// const verbIndex = genderPronouns[0] === 'They' ? 
-// let wordList = `subjectPronoun: this.props.genderPronoun, fname: this.props.student.fname, subject: this.props.class.subject...`;
-
-// let regexpNames =  /subjectPronoun: (\w+), fname: (\w+), subject: (\w+)/mg;
-// let replacerFunction = str.replaceAll(regexpNames, wordList);
-// do {
-//   console.log(`Hello ${match[1]} ${match[2]}`);
-// } while ((match = regexpNames.exec(wordList)) !== null);
 
 const subjectPronoun = genderPronouns[0];
 const objectPronoun = genderPronouns[1];
 const possessivePronoun = genderPronouns[2];
-
-// search fname
-// search subject type of class
-// search className
-// search category
 
 verbReplacement = (genderPronouns, fname, className, subject, body) => {
   const verbsRegular = ['complete', 'fulfill', 'distract', 'lack', 'show', 'miss', 'need', 'participate']
@@ -76,12 +33,12 @@ verbReplacement = (genderPronouns, fname, className, subject, body) => {
     body = body.replaceAll(concatKey, concatObj[concatKey])
   })
 
-  console.log(body)
+  // console.log(body)
 }
 
-let pronouns = ['She','Her','Her']
-let body = '%fname% has performed very unsatisfactorily in class and needs quick improvement to not only pass this class but proceed onto next level coursework in %subject%.  %subjectPronoun% &be& an active participant.  %subjectPronoun% &have& excellent attendance. %subjectPronoun% &be& very punctual.'
-let fname = 'Matt'
-let className = 'American History'
-let subject = 'History'
-console.log(verbReplacement(pronouns, body))
+// let pronouns = ['She','Her','Her']
+// let body = '%fname% has performed very unsatisfactorily in class and needs quick improvement to not only pass this class but proceed onto next level coursework in %subject%.  %subjectPronoun% &be& an active participant.  %subjectPronoun% &have& excellent attendance. %subjectPronoun% &be& very punctual.'
+// let fname = 'Matt'
+// let className = 'American History'
+// let subject = 'History'
+// console.log(verbReplacement(pronouns, body))
