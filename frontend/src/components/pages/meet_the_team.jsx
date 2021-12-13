e3a2c5b6dc947bf90d7a571f7f436f6bc9dc5e8d
cjc473 <chris.cheasty3@gmail.com>
2021-12-13 01:08:33 -0800

    basic styling for meet team page

--- frontend/src/components/pages/meet_the_team.jsx
+++ frontend/src/components/pages/meet_the_team.jsx
@@ -3,51 +3,63 @@ import "../../styles/meet-the-team.css";
 
 const MeetTheTeam = () => (
   <div className="meet-the-team">
-    <h1>Meet the Team</h1>
     <div className="team-list-container">
-      <ul className='team-members'>
-        <li>
-          <p>Julian Erville</p>
-          <img src="https://avatars.githubusercontent.com/u/79581995?v=4" alt="julian" />
-          <a href="">
-            <i class="fab fa-linkedin"></i>
-          </a>
-          <a href="https://github.com/jgerville/">
-            <i class="fab fa-github"></i>
-          </a>
-        </li>
-        <li>
-          <p>Emily Bell</p>
-          <img src="https://avatars.githubusercontent.com/u/89127270?v=4" alt="emily" />
-          <a href="">
-            <i class="fab fa-linkedin"></i>
-          </a>
-          <a href="https://github.com/ecbell">
-            <i class="fab fa-github"></i>
-          </a>
-        </li>
-        <li>
-          <p>Chris Cheasty</p>
-          <img src="https://avatars.githubusercontent.com/u/75001991?v=4" alt="chris" />
-          <a href="">
-            <i class="fab fa-linkedin"></i>
-          </a>
-          <a href="https://github.com/cjc473">
-            <i class="fab fa-github"></i>
-          </a>
-        </li>
-        <li>
-          <p>Matthew Lese</p>
-          <img src="https://avatars.githubusercontent.com/u/63416154?v=4" alt="matt" />
-          <a href="">
-            <i class="fab fa-linkedin"></i>
-          </a>
-          <a href="https://github.com/matthewlese">
-            <i class="fab fa-github"></i>
-          </a>
-        </li>
-      </ul>
+      <div className='team-members'>
+        <div className='team-col'>
+          <div className='teammate-card'>
+            <img src="https://avatars.githubusercontent.com/u/79581995?v=4" alt="julian" />
+            <p className='teammate-name'>Julian Erville</p>
+            <div className='teammate-socials'>
+              <a href="https://www.linkedin.com/">
+                <i class="fab fa-linkedin"></i>
+              </a>
+              <a href="https://github.com/jgerville/">
+                <i class="fab fa-github"></i>
+              </a>
+            </div>
+          </div>
+          <div className='teammate-card'>
+            <img src="https://avatars.githubusercontent.com/u/89127270?v=4" alt="emily" />
+            <p className='teammate-name'>Emily Bell</p>
+            <div className='teammate-socials'>
+              <a href="https://www.linkedin.com/">
+                <i class="fab fa-linkedin"></i>
+              </a>
+              <a href="https://github.com/ecbell">
+                <i class="fab fa-github"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className='team-col'>
+          <div className='teammate-card'>
+            <img src="https://avatars.githubusercontent.com/u/75001991?v=4" alt="chris" />
+            <p className='teammate-name'>Chris Cheasty</p>
+            <div className='teammate-socials'>
+              <a href="https://www.linkedin.com/in/chris-cheasty/">
+                <i class="fab fa-linkedin"></i>
+              </a>
+              <a href="https://github.com/cjc473">
+                <i class="fab fa-github"></i>
+              </a>
+            </div>
+          </div>
+          <div className='teammate-card'>
+            <img src="https://avatars.githubusercontent.com/u/63416154?v=4" alt="matt" />
+            <p className='teammate-name'>Matthew Lese</p>
+            <div className='teammate-socials'>
+              <a href="https://www.linkedin.com/in/matthewlese/">
+                <i class="fab fa-linkedin"></i>
+              </a>
+              <a href="https://github.com/matthewlese">
+                <i class="fab fa-github"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    <div id="project-desc">Teacher’s Pet is a collaboration inspired by the experiences of former English teacher Julian Erville. To see additional projects by our team members, please visit our LinkedIn and GitHub Pages</div>
   </div>
 )
 
