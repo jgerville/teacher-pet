ef88b956d7faf4e0adc1950dd22be8af11e3ca68
Julian Erville <jerville@gmail.com>
2021-12-15 21:11:08 -0500

    Add OpenGraph meta tags

--- frontend/src/components/pages/meet_the_team.jsx
+++ frontend/src/components/pages/meet_the_team.jsx
@@ -10,7 +10,7 @@ const MeetTheTeam = () => (
             <img src="https://avatars.githubusercontent.com/u/79581995?v=4" alt="julian" />
             <p className='teammate-name'>Julian Erville</p>
             <div className='teammate-socials'>
-              <a href="https://www.linkedin.com/in/julian-erville-9a8911224/">
+              <a href="https://www.linkedin.com/in/julian-erville/">
                 <i class="fab fa-linkedin"></i>
               </a>
               <a href="https://github.com/jgerville/">
