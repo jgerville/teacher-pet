9d7e82a5694f6f4d1946f1208f7d26d47f80d196
Julian Erville <jerville@gmail.com>
2021-12-16 13:54:11 -0500

    Clean up array util, make date sorting put most recent reports at the top rather than the opposite

--- frontend/src/util/array_util.js
+++ frontend/src/util/array_util.js
@@ -3,14 +3,8 @@ export const sortAlphabetically = (arrayOfObjects, propertyToSortBy) =>
     a[propertyToSortBy][0].toLowerCase() > b[propertyToSortBy][0].toLowerCase() ? 1 : -1
   );
 
-export const sortAlphabeticallyWord = (arrayOfObjects, propertyToSortBy) =>
-  arrayOfObjects.sort((a, b) => {
-    console.log(arrayOfObjects, propertyToSortBy)
-    // a[propertyToSortBy][0].toLowerCase() > b[propertyToSortBy][0].toLowerCase() ? 1 : -1
-
-  });
-
+// most recent first
 export const sortByDate = (arrayOfObjects, propertyToSortBy) =>
   arrayOfObjects.sort((a, b) =>
-    new Date(a[propertyToSortBy]) - new Date(b[propertyToSortBy])
+    new Date(b[propertyToSortBy]) - new Date(a[propertyToSortBy])
   )
