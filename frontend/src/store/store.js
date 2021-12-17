ba05b3ac0777915dde7c328b51488e3e477f84fe
Julian Erville <jerville@gmail.com>
2021-12-17 08:51:20 -0500

    Add functionality: by configuring redux-persist, the state persists to local storage and users can now refresh or use the back button without errors

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,15 +1,26 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import rootReducer from '../reducers/root_reducer';
 
-const configureStore = (preloadedState = {}) => (
-  createStore(
-    rootReducer,
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const configureStore = (preloadedState = {}) => {
+  let store = createStore(
+    persistedReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(thunk)
   )
-);
+  let persistor = persistStore(store);
+  return { store, persistor }
+};
 
 export default configureStore;
