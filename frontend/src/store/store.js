d7141339c563600673ee80e108588a5e9275438c
Julian Erville <jerville@gmail.com>
2021-12-17 10:30:06 -0500

    Remove logger

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,7 +3,7 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'
 import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import rootReducer from '../reducers/root_reducer';
 
@@ -19,7 +19,7 @@ const configureStore = (preloadedState = {}) => {
   let store = createStore(
     persistedReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(thunk)
   )
   let persistor = persistStore(store);
   return { store, persistor }
